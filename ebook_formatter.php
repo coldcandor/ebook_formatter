78dad0b4c5bb09fbbf374f9716f8676dce0d4400
Eric Shields <eshields@synacor.com>
2012-05-12 20:01:44 -0400

    v1.0.2

--- ebook_formatter.php
+++ ebook_formatter.php
@@ -2,9 +2,9 @@
 
 //include 'E:\CO-OP\Summer 2005\Work files\ASCII.php';
 
-// Input error checking and file setup
+// Input error checking and file setup.
 if(count($argv) != 4) {
-  exit("Usage:  eBookFormatter <Input File> <Output File> <Stage>\n\n");
+  exit("Usage:  eBookFormatter <Input File> <Output File> <function>\n\n");
 } else if(!$fileArray = file($argv[1])) {
   exit("Fatal Error:  Could not open input file ($argv[1])!\n\n");
 } else if(!$fp1 = fopen($argv[2], 'w')) {
@@ -14,14 +14,19 @@ if(count($argv) != 4) {
 switch($argv[3]) {
   case 1:
     
-    // Establish tags - essentially new paragraghs
+    // Establish tags - essentially new paragraghs.
     reset($fileArray);
     while(current($fileArray)) {
+      
+      // Check for evidence of a new paragraph: a blank line, a tab or multiple
+      // spaces at the start of a line.  If found, add a tag on that line.
       if(preg_match("/^[\n\t\s{2,}]/", current($fileArray))) {
         $tags[] = key($fileArray);
-      }
+      } // End if statement
+      
       next($fileArray);
-    }
+      
+    } // End while loop
     
     // Eliminate blank lines
     reset($fileArray);
@@ -30,24 +35,25 @@ switch($argv[3]) {
       next($fileArray);
     } // End while loop                           
     
-    // Setup output file
-    $cwd = getcwd();
-    $fp1 = fopen("$cwd\output2.rtf", 'w');
-    
     // Strip newlines from each line and form paragraphs as one line each (no 
-    // newlines), trim any whitespace from the start and end of each paragraph, and 
-    // output the paragraph to a file.
+    // newline characters), trim any whitespace from the start and end of each 
+    // paragraph, and output the paragraph to a file.
     reset($fileArray);
     while(current($fileArray)) {
       
       $fileString = '';
+      
+      // Strip newlines
       while(current($fileArray) && !array_search(key($fileArray), $tags)) {
         $fileString .= preg_replace("/[\n\r]+/", ' ', current($fileArray));
         next($fileArray);
       } // End while loop
     
-      $fileString = preg_replace("/^\s*(.*)\s*$/", "$1\n\n", $fileString);
+      // Trim whitespace and format paragraph
+      $fileString = preg_replace("/^\s*/", "\t", $fileString);
+      $fileString = preg_replace("/\s*$/D", "\n", $fileString);
       
+      // Output formatted string to output file
       fputs($fp1, $fileString);
       
       next($fileArray);
@@ -59,12 +65,8 @@ switch($argv[3]) {
     break;
   case 2:
   
-    $fileArray = file($argv[1]);
-    $cwd = getcwd();
-    $fp1 = fopen("$cwd\\$argv[2]", 'w');
-    
-    // Remove whitespace from each line, place a starting tab on each paragraph, and
-    // enter a newline between paragraphs.
+    // Remove whitespace from each line, place a starting tab on each paragraph,
+    // and enter a newline between paragraphs.
     reset($fileArray);
     while(current($fileArray)) {
       $fileArray[key($fileArray)] = preg_replace("/^\s*/", "\t", current($fileArray));
@@ -77,8 +79,10 @@ switch($argv[3]) {
     
     break;
   default:
-    
-    echo "Error:  Stage select value must be 1 or 2\n";
+  
+    // Close output file and error out of program.
+    fclose($fp1);  
+    exit("Error:  Stage select value must be 1 or 2\n");
     
     break;
     
